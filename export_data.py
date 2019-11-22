357a1c6d106c7b079afeb91cc1d21ea0887abe13
Johannes Meyer <meyerjo@informatik.uni-freiburg.de>
2019-11-22 13:50:24 +0100

    * export data

--- export_data.py
+++ export_data.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 import os
 import re
 
@@ -122,6 +123,33 @@ def handle_file(DIR, fname, OUTPUT_DIR):
             im = Image.fromarray(image_decode_jpg.numpy())
             im.save(_save_path)
 
+        _save_path = os.path.join(
+            output_data_folder, 'info'
+        )
+        if not os.path.exists(_save_path):
+            os.mkdir(_save_path)
+
+        data = []
+        for i, img in enumerate(frame.images):
+            velocities = {x[0].name: x[1] for x in img.velocity.ListFields()}
+            pose_data = {x[0].name: x[1] for x in img.pose.ListFields()}
+            data.append({
+                'id': i,
+                'name': img.name,
+                'velocities': velocities,
+                'pose': pose_data,
+                'shutter': img.shutter,
+                'pose_timestamp': img.pose_timestamp,
+                'camera_trigger_time': img.camera_trigger_time,
+                'camera_readout_done_time': img.camera_readout_time,
+            })
+        with open(os.path.join(
+                _save_path, 'frame_{}.json'.format(frame_id)), 'w') as f:
+            f.write(json.dumps(data))
+
+
+        if not os.path.exists(os.path.join(output_data_folder, 'lidar_rgb')):
+            os.mkdir(os.path.join(output_data_folder, 'lidar_rgb'))
         # rgb projection of images
         _images = project_image_lidar_to_image_plane(frame)
         for i, proj_img in enumerate(_images):
